bea21e55533d93466127b799e254eb00b41b6075
manbonad <114388931+manbonad@users.noreply.github.com>
2025-02-27 17:06:24 +0100

    Update nodejsDSL.groovy
    Updated information

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -2,7 +2,7 @@ job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
         git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
+            node / gitConfigName('Jules')
             node / gitConfigEmail('macloujulian@gmail.com')
         }
     }
